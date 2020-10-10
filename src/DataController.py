e632e2ed61ba4f3434811780cd91448135eae018
Yen-Tse Liao <s88037zz@gmail.com>
2020-10-10 09:42:27 +0800

    Change webdriver downlow directory

--- src/DataController.py
+++ src/DataController.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-import time
+import time, os
 
 class DataController(object):
     def __init__(self):
         self.url = "https://hk.investing.com/etfs/spdr-s-p-500-historical-data"
-        self.driver = webdriver.Chrome("./chromedriver")
+        self.driver = self.getDriver()
         self.email = "s88037zz@gmail.com"
         self.passwd = 's88037zz'
 
@@ -32,17 +32,34 @@ class DataController(object):
     def hover(self, element):
         ActionChains(self.driver).move_to_element(element).perform()
 
+    def getDriver(self):
+        options = self.getChromeOptions()
+        driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
+        return driver
+
+    def getChromeOptions(self):
+        download_dir = os.path.join(os.path.abspath(os.path.pardir), "data")
+        options = webdriver.ChromeOptions()
+        prefs = {'profile.default_content_settings.popups': 0,
+                 'download.default_directory': download_dir,
+                 'directory_upgrade': True}
+        options.add_experimental_option('prefs', prefs)
+        return options
+
     def downlowSP500(self, start_date, end_date):
         self.driver.get(self.url)
         time.sleep(2)
 
         # change date range we download
         elem_date_range = self.driver.find_element_by_xpath("//*[@id='widgetFieldDateRange']")
-        self.driver.execute_script("arguments[0].innerText = '2020/01/01 - 2020/10/09'", elem_date_range)
+        date_range = " - ".join((start_date, end_date))
+        self.driver.execute_script("arguments[0].innerText = {}".format(date_range), elem_date_range)
 
         # download data
         elem_download = self.driver.find_element_by_xpath("//*[@title='下載數據']")
         elem_download.click()
+        print("Download successfully")
+
 
 if __name__ == '__main__':
     dc = DataController()
